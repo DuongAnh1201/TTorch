36795cce92cbc0996ea2541d6b711b8e6204a29a
DuongAnh1212 <tomnguyen6766@gmail.com>
2026-04-21 21:27:58 -0700

    update(autograd): add helper and AddBackward::backward

--- src/autograd.cpp
+++ src/autograd.cpp
@@ -1,11 +1,37 @@
 // autograd.cpp - placeholder
 #include<iostream>
 #include "tensor.h"
+#include "autograd.h"
 
 using namespace std;
 
 //Core Infrastructure
+//Helper function
+static void acum_grad(Tensor* t, const Tensor& g)
+{
+    if(!t->requires_grad)
+    {
+        return;
+    }
+    if(!t->grad)
+    {
+        t->grad = new Tensor(t->shape);
+        for (double &v: t->grad->data){v = 0.0;}
+    }
+    for(int i = 0; i< (int)t->grad->data.size(); i ++)
+    {
+        t->grad->data[i] += g.data[i];
+    }
+}
 
+// ── AddBackward ──────────────────────────────────────────────
+// forward:  c = a + b
+// backward: grad flows to both a and b unchanged
+void AddBackward::backward(Tensor& g)
+{
+    acum_grad(inputs[1], g);
+    acum_grad(inputs[1], g);
+}
 //Gradient Functions
 
 //Backward Engine
