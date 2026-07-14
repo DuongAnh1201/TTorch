b1c67ea11ba6ed089e328ee511caf307d7e15056
DuongAnh1212 <tomnguyen6766@gmail.com>
2026-07-14 10:58:28 -0700

    update(autograd): fix bugs never push the node to order at the end

--- src/autograd.cpp
+++ src/autograd.cpp
@@ -34,6 +34,7 @@ static void topo_sort(Tensor* t, unordered_set<Tensor*>& visited, vector<Tensor*
     {
         topo_sort(input, visited, order);
     }
+    order.push_back(t)
 }
 void Tensor::backward()
 {
